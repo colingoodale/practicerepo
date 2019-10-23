f09ec626ad8e83f065c0a7244350f255cc1784c1
Colin <colingoodale@gmail.com>
2019-10-23 11:01:46 -0500

    new addition

--- public/js/members.js
+++ public/js/members.js
@@ -5,5 +5,16 @@ $(document).ready(function () {
     console.log(data);
     $(".member-name").text(data.email);
     $("#diet").text(data.diet);
+    getRecipe(data.diet);
   });
+  function getRecipe(diet) {
+    var queryURL = "https://api.edamam.com/search?q=tofu&app_id=edamam-recipe-search.p.rapidapi.com&app_key=5608155f22msh0134e91f7377536p10082bjsn69ced82abbca&health=" + diet;
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
+    })
+  }
+
 });
